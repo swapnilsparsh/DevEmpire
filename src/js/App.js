eb2bd2b8ac1447e2315d465e4eef01023e5ca5d4
jatiinyadav <jatin27yadav@gmail.com>
2021-08-11 20:41:54 +0530

    Updates

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Content from "./main/content";
+import Content from "../main/content";
 
 const App = () => {
   return (
--- src/main/content.js
+++ src/main/content.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../App.css";
+import "../css/App.css";
 import Footer from "../Footer/footer";
 
 const Content = () => (
@@ -8,11 +8,13 @@ const Content = () => (
     <div className="container">
       <div className="card">
         <div className="content">
-          <h3>
-            Google Developer Student Clubs
-          </h3>
+          <h3>Google Developer Student Clubs</h3>
           <img src={"../img/dsc.png"} alt="" />
-          <a href="https://developers.google.com/community/dsc" target="_blank" rel="noreferrer">
+          <a
+            href="https://developers.google.com/community/dsc"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -22,7 +24,11 @@ const Content = () => (
         <div className="content">
           <h3>Microsoft Learn Student Ambassador</h3>
           <img src="../img/mlsa.png" alt="" />
-          <a href="https://studentambassadors.microsoft.com/" target="_blank" rel="noreferrer">
+          <a
+            href="https://studentambassadors.microsoft.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -32,7 +38,11 @@ const Content = () => (
         <div className="content">
           <h3 className="heading">Github Campus Expert</h3>
           <img src="../img/Github.png" alt="" />
-          <a href="https://education.github.com/experts" target="_blank" rel="noreferrer">
+          <a
+            href="https://education.github.com/experts"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -42,7 +52,11 @@ const Content = () => (
         <div className="content">
           <h3>Unity Student Ambassador Program</h3>
           <img src="../img/Unity.png" alt="" />
-          <a href="https://unity.com/learn/student-ambassadors" target="_blank" rel="noreferrer">
+          <a
+            href="https://unity.com/learn/student-ambassadors"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -52,7 +66,11 @@ const Content = () => (
         <div className="content">
           <h3>OnePlus Student Ambassador Program</h3>
           <img src="../img/OnePlus.jpg" alt="" />
-          <a href="https://www.oneplus.in/campus" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.oneplus.in/campus"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -62,7 +80,11 @@ const Content = () => (
         <div className="content">
           <h3>HackerEarth Campus Ambassador</h3>
           <img src="../img/Hackerearth.jpg" alt="" />
-          <a href="https://www.hackerearth.com/university/intro/" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.hackerearth.com/university/intro/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -72,7 +94,11 @@ const Content = () => (
         <div className="content">
           <h3 className="heading"> Mozilla Campus Club</h3>
           <img src="../img/Mozilla.png" alt="" />
-          <a href="https://campus.mozilla.community/" target="_blank" rel="noreferrer">
+          <a
+            href="https://campus.mozilla.community/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -82,7 +108,11 @@ const Content = () => (
         <div className="content">
           <h3>Codecademy Chapter Leader</h3>
           <img src="../img/CodeAcademy.jpg" alt="" />
-          <a href="https://community.codecademy.com/chapters/" target="_blank" rel="noreferrer">
+          <a
+            href="https://community.codecademy.com/chapters/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -92,7 +122,11 @@ const Content = () => (
         <div className="content">
           <h3 className="heading">Auth0 Ambassador</h3>
           <img src="../img/Auth0.jpg" alt="" />
-          <a href="https://auth0.com/ambassador-program" target="_blank" rel="noreferrer">
+          <a
+            href="https://auth0.com/ambassador-program"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -100,9 +134,13 @@ const Content = () => (
 
       <div className="card">
         <div className="content">
-          <h3 className="heading">Postman Student Expert</h3>
+          <h3>Postman Student Expert</h3>
           <img src="../img/Postman.png" alt="" />
-          <a href="https://www.postman.com/company/student-program/" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.postman.com/company/student-program/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -112,7 +150,11 @@ const Content = () => (
         <div className="content">
           <h3>Developer Circles from Facebook</h3>
           <img className="image" src="../img/Facebook.png" alt="" />
-          <a href="https://developers.facebook.com/developercircles/" target="_blank" rel="noreferrer">
+          <a
+            href="https://developers.facebook.com/developercircles/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -121,10 +163,14 @@ const Content = () => (
       <div className="card">
         <div className="content">
           <h3>GeeksforGeeks Campus Ambassador</h3>
-          <img className="image" src="../img/GeeksForGeeks.png" alt=""/>
-            <a href="https://www.geeksforgeeks.org/campus-ambassador-program-by-geeksforgeeks/" target="_blank" rel="noreferrer">
-              Read More
-            </a>
+          <img className="image" src="../img/GeeksForGeeks.png" alt="" />
+          <a
+            href="https://www.geeksforgeeks.org/campus-ambassador-program-by-geeksforgeeks/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Read More
+          </a>
         </div>
       </div>
 
@@ -140,9 +186,13 @@ const Content = () => (
 
       <div className="card">
         <div className="content">
-          <h3 className="heading">CodeChef College Chapter</h3>
+          <h3>CodeChef College Chapter</h3>
           <img src="../img/Codechef.png" alt="" />
-          <a href="https://www.codechef.com/college-chapter/about" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.codechef.com/college-chapter/about"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -150,9 +200,13 @@ const Content = () => (
 
       <div className="card">
         <div className="content">
-          <h3 className="heading">Intel Student Ambassador</h3>
+          <h3>Intel Student Ambassador</h3>
           <img src="../img/Intel.jpeg" alt="" />
-          <a href="https://software.intel.com/content/www/us/en/develop/community/ambassadors.html" target="_blank" rel="noreferrer" >
+          <a
+            href="https://software.intel.com/content/www/us/en/develop/community/ambassadors.html"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
@@ -162,7 +216,11 @@ const Content = () => (
         <div className="content">
           <h3>Cisco Campus Ambassador Program</h3>
           <img src="../img/Cisco.jpg" alt="" />
-          <a href="https://app.smartsheet.com/b/form/a92f74ca555f44e4aa638a4f00a77213" target="_blank" rel="noreferrer" >
+          <a
+            href="https://app.smartsheet.com/b/form/a92f74ca555f44e4aa638a4f00a77213"
+            target="_blank"
+            rel="noreferrer"
+          >
             Read More
           </a>
         </div>
