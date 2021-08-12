e8c6511141b0968a93c543f82b38e4efbd560508
swapnilsparsh <arezona.lucky55@gmail.com>
2021-08-12 14:26:00 +0530

    Programs Added

--- src/main/content.js
+++ src/main/content.js
@@ -177,7 +177,7 @@ const Content = () => (
       <div className="card">
         <div className="content">
           <h3 className="heading">MLH Fellowship</h3>
-          <img className="image" src="../img/MLH.jpg" alt="" />
+          <img className="image" src="../img/MLHFellowship.jpg" alt="" />
           <a href="https://fellowship.mlh.io/" target="_blank" rel="noreferrer">
             Read More
           </a>
@@ -227,14 +227,126 @@ const Content = () => (
       </div>
       <div class="card">
         <div class="content">
-          <h3></h3>
-          <a href="#">Read More</a>
+          <h3>Cloud Native Ambassador</h3>
+          <img src="../img/CNCF.png"  alt="" ></img>
+          <a href="https://www.cncf.io/people/ambassadors/">Read More</a>
         </div>
       </div>
       <div class="card">
         <div class="content">
-          <h3></h3>
-          <a href="#">Read More</a>
+          <h3>MongoDB Community Leader</h3>
+          <img src="../img/MongoDB.png"  alt="" ></img>
+          <a href="https://live.mongodb.com/start-a-group/" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>Prisma Ambassador</h3>
+          <img src="../img/Prisma.png"  alt="" ></img>
+          <a href="https://www.prisma.io/ambassador" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>TED Fellows Program</h3>
+          <img src="../img/TedFellow.jpg"  alt="" ></img>
+          <a href="https://www.ted.com/participate/ted-fellows-program" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>TensorFlow User Group</h3>
+          <img src="../img/TensorFlow.png"  alt="" ></img>
+          <a href="https://www.tensorflow.org/community/groups" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>Google Developer Groups Organizer</h3>
+          <img src="../img/GDG.png" alt="" ></img>
+          <a href="https://developers.google.com/community/gdg/organizers" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>Women Techmakers Ambassador</h3>
+          <img src="../img/WTM.jpg"  alt="" ></img>
+          <a href="https://www.womentechmakers.com/ambassadors" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>MLH Member Event</h3>
+          <img src="../img/MLH.png"  alt="" ></img>
+          <a href="https://mlh.io/event-membership" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>Kotlin User Group</h3>
+          <img src="../img/Kotlin.png"  alt="" ></img>
+          <a href="https://kotlinlang.org/user-groups/user-group-list.html" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>Alexa Student Influencer</h3>
+          <img src="../img/Alexa.png"  alt="" ></img>
+          <a href="https://developer.amazon.com/en-IN/alexa/alexa-student-influencer#" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>GitLab Hero</h3>
+          <img src="../img/Gitlab.png"  alt="" ></img>
+          <a href="https://about.gitlab.com/community/heroes/" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>GitKraken Ambassador</h3>
+          <img src="../img/GitKraken.jpg"  alt="" ></img>
+          <a href="https://www.gitkraken.com/ambassador" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>UiPath Student Developer Program</h3>
+          <img src="../img/UiPath.png"  alt="" ></img>
+          <a href="https://community.uipath.com/uipath-student-developers-program/" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <h3>IBM ZAMBASSADOR</h3>
+          <img src="../img/IBM.jpg"  alt="" ></img>
+          <a href="https://zambassador.com/" target="_blank" rel="noreferrer">
+            Read More
+          </a>
         </div>
       </div>
     </div>
