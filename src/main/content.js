7b03b56c39c452d81a73c4ec4111d42525b7ea1c
swapnilsparsh <arezona.lucky55@gmail.com>
2021-08-12 14:58:52 +0530

    Arranged Alphabetically

--- src/main/content.js
+++ src/main/content.js
@@ -6,113 +6,11 @@ const Content = () => (
   <div>
     {/* <img className="background" src="../img/Developer-Society-bg.png" alt="" /> */}
     <div className="container">
-      <div className="card">
-        <div className="content">
-          <h3>Google Developer Student Clubs</h3>
-          <img src={"../img/dsc.png"} alt="" />
-          <a
-            href="https://developers.google.com/community/dsc"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="content">
-          <h3>Microsoft Learn Student Ambassador</h3>
-          <img src="../img/mlsa.png" alt="" />
-          <a
-            href="https://studentambassadors.microsoft.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="content">
-          <h3 className="heading">Github Campus Expert</h3>
-          <img src="../img/Github.png" alt="" />
-          <a
-            href="https://education.github.com/experts"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="content">
-          <h3>Unity Student Ambassador Program</h3>
-          <img src="../img/Unity.png" alt="" />
-          <a
-            href="https://unity.com/learn/student-ambassadors"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="content">
-          <h3>OnePlus Student Ambassador Program</h3>
-          <img src="../img/OnePlus.jpg" alt="" />
-          <a
-            href="https://www.oneplus.in/campus"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="content">
-          <h3>HackerEarth Campus Ambassador</h3>
-          <img src="../img/Hackerearth.jpg" alt="" />
-          <a
-            href="https://www.hackerearth.com/university/intro/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="content">
-          <h3 className="heading"> Mozilla Campus Club</h3>
-          <img src="../img/Mozilla.png" alt="" />
-          <a
-            href="https://campus.mozilla.community/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="content">
-          <h3>Codecademy Chapter Leader</h3>
-          <img src="../img/CodeAcademy.jpg" alt="" />
-          <a
-            href="https://community.codecademy.com/chapters/"
-            target="_blank"
-            rel="noreferrer"
-          >
+      <div class="card">
+        <div class="content">
+          <h3>Alexa Student Influencer</h3>
+          <img src="../img/Alexa.png"  alt="" ></img>
+          <a href="https://developer.amazon.com/en-IN/alexa/alexa-student-influencer#" target="_blank" rel="noreferrer">
             Read More
           </a>
         </div>
@@ -122,11 +20,7 @@ const Content = () => (
         <div className="content">
           <h3 className="heading">Auth0 Ambassador</h3>
           <img src="../img/Auth0.jpg" alt="" />
-          <a
-            href="https://auth0.com/ambassador-program"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://auth0.com/ambassador-program" target="_blank" rel="noreferrer">
             Read More
           </a>
         </div>
@@ -134,13 +28,39 @@ const Content = () => (
 
       <div className="card">
         <div className="content">
-          <h3>Postman Student Expert</h3>
-          <img src="../img/Postman.png" alt="" />
-          <a
-            href="https://www.postman.com/company/student-program/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <h3>Cisco Campus Ambassador Program</h3>
+          <img src="../img/Cisco.jpg" alt="" />
+          <a href="https://app.smartsheet.com/b/form/a92f74ca555f44e4aa638a4f00a77213" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h3>Cloud Native Ambassador</h3>
+          <img src="../img/CNCF.png"  alt="" ></img>
+          <a href="https://www.cncf.io/people/ambassadors/">
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="content">
+          <h3>Codecademy Chapter Leader</h3>
+          <img src="../img/Codecademy.jpg" alt="" />
+          <a href="https://community.codecademy.com/chapters/" target="_blank" rel="noreferrer" >
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="content">
+          <h3>CodeChef College Chapter</h3>
+          <img src="../img/Codechef.png" alt="" />
+          <a href="https://www.codechef.com/college-chapter/about" target="_blank" rel="noreferrer" >
             Read More
           </a>
         </div>
@@ -150,11 +70,27 @@ const Content = () => (
         <div className="content">
           <h3>Developer Circles from Facebook</h3>
           <img className="image" src="../img/Facebook.png" alt="" />
-          <a
-            href="https://developers.facebook.com/developercircles/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://developers.facebook.com/developercircles/" target="_blank" rel="noreferrer" >
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h3>Google Developer Groups Organizer</h3>
+          <img src="../img/GDG.png" alt="" ></img>
+          <a href="https://developers.google.com/community/gdg/organizers" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="content">
+          <h3>Google Developer Student Clubs</h3>
+          <img src={"../img/GDSC.png"} alt="" />
+          <a href="https://developers.google.com/community/dsc" target="_blank" rel="noreferrer">
             Read More
           </a>
         </div>
@@ -164,11 +100,87 @@ const Content = () => (
         <div className="content">
           <h3>GeeksforGeeks Campus Ambassador</h3>
           <img className="image" src="../img/GeeksForGeeks.png" alt="" />
-          <a
-            href="https://www.geeksforgeeks.org/campus-ambassador-program-by-geeksforgeeks/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://www.geeksforgeeks.org/campus-ambassador-program-by-geeksforgeeks/" target="_blank" rel="noreferrer" >
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="content">
+          <h3 className="heading">Github Campus Expert</h3>
+          <img src="../img/Github.png" alt="" />
+          <a href="https://education.github.com/experts" target="_blank" rel="noreferrer" >
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h3>GitKraken Ambassador</h3>
+          <img src="../img/GitKraken.jpg"  alt="" ></img>
+          <a href="https://www.gitkraken.com/ambassador" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h3>GitLab Hero</h3>
+          <img src="../img/Gitlab.png"  alt="" ></img>
+          <a href="https://about.gitlab.com/community/heroes/" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="content">
+          <h3>HackerEarth Campus Ambassador</h3>
+          <img src="../img/Hackerearth.jpg" alt="" />
+          <a href="https://www.hackerearth.com/university/intro/" rel="noreferrer" >
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h3>IBM ZAMBASSADOR</h3>
+          <img src="../img/IBM.jpg"  alt="" ></img>
+          <a href="https://zambassador.com/" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="content">
+          <h3>Intel Student Ambassador</h3>
+          <img src="../img/Intel.jpeg" alt="" />
+          <a href="https://software.intel.com/content/www/us/en/develop/community/ambassadors.html" target="_blank" rel="noreferrer" >
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h3>Kotlin User Group</h3>
+          <img src="../img/Kotlin.png"  alt="" ></img>
+          <a href="https://kotlinlang.org/user-groups/user-group-list.html" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="content">
+          <h3>Microsoft Learn Student Ambassador</h3>
+          <img src="../img/mlsa.png" alt="" />
+          <a href="https://studentambassadors.microsoft.com/" target="_blank" rel="noreferrer" >
             Read More
           </a>
         </div>
@@ -184,108 +196,6 @@ const Content = () => (
         </div>
       </div>
 
-      <div className="card">
-        <div className="content">
-          <h3>CodeChef College Chapter</h3>
-          <img src="../img/Codechef.png" alt="" />
-          <a
-            href="https://www.codechef.com/college-chapter/about"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="content">
-          <h3>Intel Student Ambassador</h3>
-          <img src="../img/Intel.jpeg" alt="" />
-          <a
-            href="https://software.intel.com/content/www/us/en/develop/community/ambassadors.html"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="content">
-          <h3>Cisco Campus Ambassador Program</h3>
-          <img src="../img/Cisco.jpg" alt="" />
-          <a
-            href="https://app.smartsheet.com/b/form/a92f74ca555f44e4aa638a4f00a77213"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-      <div class="card">
-        <div class="content">
-          <h3>Cloud Native Ambassador</h3>
-          <img src="../img/CNCF.png"  alt="" ></img>
-          <a href="https://www.cncf.io/people/ambassadors/">Read More</a>
-        </div>
-      </div>
-      <div class="card">
-        <div class="content">
-          <h3>MongoDB Community Leader</h3>
-          <img src="../img/MongoDB.png"  alt="" ></img>
-          <a href="https://live.mongodb.com/start-a-group/" target="_blank" rel="noreferrer">
-            Read More
-          </a>
-        </div>
-      </div>
-      <div class="card">
-        <div class="content">
-          <h3>Prisma Ambassador</h3>
-          <img src="../img/Prisma.png"  alt="" ></img>
-          <a href="https://www.prisma.io/ambassador" target="_blank" rel="noreferrer">
-            Read More
-          </a>
-        </div>
-      </div>
-      <div class="card">
-        <div class="content">
-          <h3>TED Fellows Program</h3>
-          <img src="../img/TedFellow.jpg"  alt="" ></img>
-          <a href="https://www.ted.com/participate/ted-fellows-program" target="_blank" rel="noreferrer">
-            Read More
-          </a>
-        </div>
-      </div>
-      <div class="card">
-        <div class="content">
-          <h3>TensorFlow User Group</h3>
-          <img src="../img/TensorFlow.png"  alt="" ></img>
-          <a href="https://www.tensorflow.org/community/groups" target="_blank" rel="noreferrer">
-            Read More
-          </a>
-        </div>
-      </div>
-      <div class="card">
-        <div class="content">
-          <h3>Google Developer Groups Organizer</h3>
-          <img src="../img/GDG.png" alt="" ></img>
-          <a href="https://developers.google.com/community/gdg/organizers" target="_blank" rel="noreferrer">
-            Read More
-          </a>
-        </div>
-      </div>
-      <div class="card">
-        <div class="content">
-          <h3>Women Techmakers Ambassador</h3>
-          <img src="../img/WTM.jpg"  alt="" ></img>
-          <a href="https://www.womentechmakers.com/ambassadors" target="_blank" rel="noreferrer">
-            Read More
-          </a>
-        </div>
-      </div>
       <div class="card">
         <div class="content">
           <h3>MLH Member Event</h3>
@@ -295,42 +205,77 @@ const Content = () => (
           </a>
         </div>
       </div>
+
       <div class="card">
         <div class="content">
-          <h3>Kotlin User Group</h3>
-          <img src="../img/Kotlin.png"  alt="" ></img>
-          <a href="https://kotlinlang.org/user-groups/user-group-list.html" target="_blank" rel="noreferrer">
+          <h3>MongoDB Community Leader</h3>
+          <img src="../img/MongoDB.png"  alt="" ></img>
+          <a href="https://live.mongodb.com/start-a-group/" target="_blank" rel="noreferrer">
             Read More
           </a>
         </div>
       </div>
-      <div class="card">
-        <div class="content">
-          <h3>Alexa Student Influencer</h3>
-          <img src="../img/Alexa.png"  alt="" ></img>
-          <a href="https://developer.amazon.com/en-IN/alexa/alexa-student-influencer#" target="_blank" rel="noreferrer">
+      
+      <div className="card">
+        <div className="content">
+          <h3 className="heading"> Mozilla Campus Club</h3>
+          <img src="../img/Mozilla.png" alt="" />
+          <a href="https://campus.mozilla.community/" target="_blank" rel="noreferrer" >
             Read More
           </a>
         </div>
       </div>
-      <div class="card">
-        <div class="content">
-          <h3>GitLab Hero</h3>
-          <img src="../img/Gitlab.png"  alt="" ></img>
-          <a href="https://about.gitlab.com/community/heroes/" target="_blank" rel="noreferrer">
+
+      <div className="card">
+        <div className="content">
+          <h3>OnePlus Student Ambassador Program</h3>
+          <img src="../img/OnePlus.jpg" alt="" />
+          <a href="https://www.oneplus.in/campus" target="_blank" rel="noreferrer" >
             Read More
           </a>
         </div>
       </div>
-      <div class="card">
-        <div class="content">
-          <h3>GitKraken Ambassador</h3>
-          <img src="../img/GitKraken.jpg"  alt="" ></img>
-          <a href="https://www.gitkraken.com/ambassador" target="_blank" rel="noreferrer">
+
+      <div className="card">
+        <div className="content">
+          <h3>Postman Student Expert</h3>
+          <img src="../img/Postman.png" alt="" />
+          <a href="https://www.postman.com/company/student-program/" target="_blank" rel="noreferrer" >
             Read More
           </a>
         </div>
       </div>
+
+      <div class="card">
+        <div class="content">
+          <h3>Prisma Ambassador</h3>
+          <img src="../img/Prisma.png"  alt="" ></img>
+          <a href="https://www.prisma.io/ambassador" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h3>TED Fellows Program</h3>
+          <img src="../img/TedFellow.jpg"  alt="" ></img>
+          <a href="https://www.ted.com/participate/ted-fellows-program" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="content">
+          <h3>TensorFlow User Group</h3>
+          <img src="../img/TensorFlow.png"  alt="" ></img>
+          <a href="https://www.tensorflow.org/community/groups" target="_blank" rel="noreferrer">
+            Read More
+          </a>
+        </div>
+      </div>
+
       <div class="card">
         <div class="content">
           <h3>UiPath Student Developer Program</h3>
@@ -340,11 +285,22 @@ const Content = () => (
           </a>
         </div>
       </div>
+
+      <div className="card">
+        <div className="content">
+          <h3>Unity Student Ambassador Program</h3>
+          <img src="../img/Unity.png" alt="" />
+          <a href="https://unity.com/learn/student-ambassadors" target="_blank" rel="noreferrer" >
+            Read More
+          </a>
+        </div>
+      </div>
+
       <div class="card">
         <div class="content">
-          <h3>IBM ZAMBASSADOR</h3>
-          <img src="../img/IBM.jpg"  alt="" ></img>
-          <a href="https://zambassador.com/" target="_blank" rel="noreferrer">
+          <h3>Women Techmakers Ambassador</h3>
+          <img src="../img/WTM.jpg"  alt="" ></img>
+          <a href="https://www.womentechmakers.com/ambassadors" target="_blank" rel="noreferrer">
             Read More
           </a>
         </div>
