a68506cf7e2b04729a2bc2a540c719b6cc41012d
swapnilsparsh <arezona.lucky55@gmail.com>
2021-09-01 18:25:45 +0530

    Star Github added

--- src/js/landingpage.js
+++ src/js/landingpage.js
@@ -3,19 +3,26 @@ import React from "react";
 const Landingpage = () => {
   return (
     <div className="container-landing">
-      <div className="top-navbar">
-        <div className="navigation">
-          <div className="dark-mode"></div>
-          <div className="twitter-button">
-            <a
-              className="twitter"
-              href="https://twitter.com/intent/tweet?text=Check%20out%20this%20awesome%20website%20%F0%9F%91%8D&url=https://github.com/swapnilsparsh/DevSociety&via=swapnilsparsh"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src="../img/twitter-share.png" alt="" />
-            </a>
-          </div>
+      <div className="navigation">
+        <div className="github-button">
+          <a
+            className="github"
+            href="/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="../img/github-share.png" alt="" />
+          </a>
+        </div>
+        <div className="twitter-button">
+          <a
+            className="twitter"
+            href="https://twitter.com/intent/tweet?text=Check%20out%20this%20awesome%20website%20%F0%9F%91%8D&url=https://github.com/swapnilsparsh/DevSociety&via=swapnilsparsh"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="../img/twitter-share.png" alt="" />
+          </a>
         </div>
       </div>
       <div className="landing-page-header">
