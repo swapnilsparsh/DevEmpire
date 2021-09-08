58d8a4771499a5152261b8245e6d334ff18c653f
jatiinyadav <jatin27yadav@gmail.com>
2021-09-08 16:52:27 +0530

    Landing Page Styled

--- src/js/landingpage.js
+++ src/js/landingpage.js
@@ -5,12 +5,7 @@ const Landingpage = () => {
     <div className="container-landing">
       <div className="navigation">
         <div className="github-button">
-          <a
-            className="github"
-            href="/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a className="github" href="/" target="_blank" rel="noreferrer">
             <img src="../img/github-share.png" alt="" />
           </a>
         </div>
@@ -26,7 +21,17 @@ const Landingpage = () => {
         </div>
       </div>
       <div className="landing-page-header">
-        <h1>Developer Society</h1>
+        <div className="header-details">
+          <h1>Developer Society</h1>
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts. Separated they
+            live in Bookmarksgrove right at the coast of the Semantics.
+          </p>
+          <a href="/">
+            <h4>Developer Society</h4>
+          </a>
+        </div>
         <img src="../img/prizegif.gif" alt="" />
       </div>
     </div>
