3b1a8d2fcad0b32df3273b5334ff073b59c731a6
Tanmaykk <tanmaykachroo@gmail.com>
2021-10-01 22:05:06 +0530

    Added label filtering for web dev resources

--- src/components/web-dev/Web-dev.js
+++ src/components/web-dev/Web-dev.js
@@ -3,61 +3,108 @@ import { useState, React } from "react";
 import { Link } from "react-scroll";
 
 const Content = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-    return (
-        <>
-            <div className="container-landing">
-                <div className="landing-page-header">
-                    <div className="header-details">
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedLabel, setSelectedLabel] = useState("");
+  const labels = [
+    ...new Set(
+      Datas.map((data) => {
+        if (!data.label) return null;
+        return data.label;
+      })
+    ),
+  ].filter((label) => label);
+  return (
+    <>
+      <div className="container-landing">
+        <div className="landing-page-header">
+          <div className="header-details">
+            <h1>Web Dev Tools</h1>
 
-                        <h1>Web Dev Tools</h1>
+            <Link to="container" smooth={true} duration={1000}>
+              <h4>Explore all</h4>
+            </Link>
 
-                        <Link to="container" smooth={true} duration={1000}>
-                            <h4>Explore all</h4>
-                        </Link>
+            <input
+              className="search"
+              text="type"
+              placeholder="Search"
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div className="label-container">
+          <span
+            className="label"
+            style={
+              selectedLabel === ""
+                ? { color: "white", background: "orange", border: "none" }
+                : {}
+            }
+            onClick={() => {
+              setSelectedLabel("");
+            }}
+          >
+            All
+          </span>
+          {labels.map((label) => {
+            return (
+              <span
+                className="label"
+                style={
+                  selectedLabel === label
+                    ? { color: "white", background: "#0093ed" }
+                    : {}
+                }
+                onClick={() => {
+                  setSelectedLabel(label);
+                }}
+              >
+                {label}
+              </span>
+            );
+          })}
+        </div>
+      </div>
 
-                        <input
-                            className="search"
-                            text="type"
-                            placeholder="Search"
-                            onChange={(event) => {
-                                setSearchTerm(event.target.value);
-                            }}
-                        />
+      <div className="container" id="container">
+        <div className="align-flex">
+          {Datas.filter((data) => {
+            if (selectedLabel === "") return data;
+            else if (data.label === selectedLabel) {
+              return data;
+            }
+          })
+            .filter((data) => {
+              if (searchTerm === "") {
+                return data;
+              } else if (
+                data.head.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return data;
+              }
+            })
+            .map((data) => {
+              return (
+                <div className="frame-border">
+                  <div className="pointer"></div>
+                  <div class="card-js">
+                    <div class="content">
+                      <a href={data.link} target="_blank" rel="noreferrer">
+                        <h3>{data.head}</h3>
+                        <img src={data.image} alt={data.alt}></img>
+                      </a>
+                      <p>{data.about}</p>
                     </div>
+                  </div>
                 </div>
-            </div>
-
-            <div className="container" id="container">
-                <div className="align-flex">
-                    {Datas.filter((data) => {
-                        if (searchTerm === "") {
-                            return data;
-                        } else if (
-                            data.head.toLowerCase().includes(searchTerm.toLowerCase())
-                        ) {
-                            return data;
-                        }
-                    }).map((data) => {
-                        return (
-                            <div className="frame-border">
-                                <div className="pointer"></div>
-                                <div class="card-js">
-                                    <div class="content">
-                                        <a href={data.link} target="_blank" rel="noreferrer">
-                                            <h3>{data.head}</h3>
-                                            <img src={data.image} alt={data.alt}></img>
-                                        </a>
-                                        <p>{data.about}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-        </>
-    );
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Content;
