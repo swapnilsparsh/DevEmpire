f5fc0155e85f2c4a5743aaed39d9ce5b38b826bd
Vaibhav <thevaibhavverma@gmail.com>
2021-10-08 20:36:21 +0530

    fix_for_issue_64

--- src/components/navbar/Links.js
+++ src/components/navbar/Links.js
@@ -5,19 +5,19 @@ const Links = () => {
   return (
     <div>
       <NavLink exact to="/" activeClassName="current-nav-link">
-        <i class="fa fa-home"></i> Home
+        <i class="fa fa-home"></i>&nbsp; Home
       </NavLink>
       <NavLink to="/ambassador" activeClassName="current-nav-link">
-        <i class="fa fa-users"></i> Ambassador
+        <i class="fa fa-users"></i>&nbsp; Ambassador
       </NavLink>
       <NavLink to="/programs" activeClassName="current-nav-link">
-        <i class="fa fa-calendar"></i> Programs
+        <i class="fa fa-calendar"></i>&nbsp; Programs
       </NavLink>
       <NavLink to="/webdev" activeClassName="current-nav-link">
-        <i class="fa fa-code"></i> Web Dev
+        <i class="fa fa-code"></i>&nbsp; Web Dev
       </NavLink>
       <NavLink to="/jstoolkits" activeClassName="current-nav-link">
-        <i class="fa fa-wrench"></i> JS Toolkits
+        <i class="fa fa-wrench"></i>&nbsp; JS Toolkits
       </NavLink>
     </div>
   );
