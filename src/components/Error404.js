75c250a5dfabc152405729617dd441c074854e3e
swapnilsparsh <arezona.lucky55@gmail.com>
2021-10-10 13:20:39 +0530

    Error.js Fix

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import Error from "../assets/svg/Error.svg";
 
 const Error404 = () => {
   return (
     <div className="error-page">
       <h2>Error 404!  Page Not Found!</h2>
-      <img src="assets/error.svg" className="error-svg" />
+      <img src={Error} alt="ErrorSVG" className="error-svg" />
       <Link to="/" className="call-to-action-btn error-back-to-home-btn">
         Back to Home
       </Link>
