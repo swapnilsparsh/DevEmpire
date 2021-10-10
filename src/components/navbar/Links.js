22e100809b06986d218cdc0117e3d6720ca24ca0
Chris Jones <zorlanv@gmail.com>
2021-10-10 16:22:27 +1100

    remove non breaking spaces

--- src/components/navbar/Links.js
+++ src/components/navbar/Links.js
@@ -5,19 +5,19 @@ const Links = () => {
   return (
     <div className="links-container">
       <NavLink exact to="/" activeClassName="current-nav-link">
-        <i class="fa fa-home"></i> &nbsp; Home
+        <i class="fa fa-home"></i> Home
       </NavLink>
       <NavLink to="/ambassador" activeClassName="current-nav-link">
-        <i class="fa fa-users"></i> &nbsp; Ambassador
+        <i class="fa fa-users"></i> Ambassador
       </NavLink>
       <NavLink to="/programs" activeClassName="current-nav-link">
-        <i class="fa fa-calendar"></i> &nbsp; Programs
+        <i class="fa fa-calendar"></i> Programs
       </NavLink>
       <NavLink to="/webdev" activeClassName="current-nav-link">
-        <i class="fa fa-code"></i> &nbsp; Web Dev
+        <i class="fa fa-code"></i> Web Dev
       </NavLink>
       <NavLink to="/jstoolkits" activeClassName="current-nav-link">
-        <i class="fa fa-wrench"></i> &nbsp; JS Toolkits
+        <i class="fa fa-wrench"></i> JS Toolkits
       </NavLink>
     </div>
   );
