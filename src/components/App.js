8eedba187aa6ffc2e779c5805a783d5177d578bf
Ashish Kashyap <akk312000@gmail.com>
2021-10-13 19:30:06 +0530

    Added Preloader to Website

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../sass/style.css";
 import Footer from "./Footer";
 import BackToTop from "./Backtotop";
+import { css } from "@emotion/react";
+
 import Navbarhere from "./Menu";
-
-
+import HashLoader from "react-spinners/HashLoader";
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+  text-align: center;
+  display: flex;
+  
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
 const App = () => {
-  return (
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  }, []);
+  return loading ? (
+    <HashLoader color={"#40aaff"} loading={loading} css={override} size={80} />
+  ) : (
     <>
       <Navbarhere />
       <BackToTop />
