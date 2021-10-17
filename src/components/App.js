e527a245f78623ad7b72a0dc4a9eb61732bfa343
Jana Todakova <jana.todakova@student.tuke.sk>
2021-10-17 22:39:36 +0200

    added preloader

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../sass/style.css";
 import Footer from "./Footer";
 import BackToTop from "./Backtotop";
 import Navbar from "./Menu";
+import Preloader from "./Preloader";
 
 
 const App = () => {
-  return (
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3200);
+  }, []);
+
+  return loading ? (
+    <Preloader />
+    ) : (
     <>
       <Navbar />
       <BackToTop />
