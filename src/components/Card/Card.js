3ad5c1ca3f9a64e9c28a37a49b547f11a479cbe2
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-02-13 13:32:19 +0530

    Created Card Component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Card({ head, link, image, alt, about }) {
+  return (
+    <div className="frame-border" key={`${head}_${link}`}>
+      <div className="pointer"></div>
+      <div className="card-js">
+        <div className="content">
+          <a href={link} target="_blank" rel="noreferrer">
+            <h3>{head}</h3>
+            <img src={image} alt={alt}></img>
+          </a>
+          <p>{about}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
