d1c0fdb3b9fa05f02269b421748539006d591881
swapnilsparsh <arezona.lucky55@gmail.com>
2022-02-14 00:12:25 +0530

    GTag Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,44 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag'
+
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Layout>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />;
     </Layout>
   );
