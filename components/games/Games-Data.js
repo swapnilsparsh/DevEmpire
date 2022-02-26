62f55f4bb0978e42f01ccd1e73480c294a1e4b59
swapnilsparsh <arezona.lucky55@gmail.com>
2022-02-26 23:01:48 +0530

    Game Section Added

---
+++ components/games/Games-Data.js
@@ -0,0 +1,130 @@
+const Games= [
+    {
+        link: "https://type.method.ac/",
+        head: "Kern Type",
+        image: "../assets/games-img/KernType.png",
+        about: "A game to learn how to kern type.",
+        alt: "Kern Type"
+    },
+   {
+        link: "https://shape.method.ac/",
+        head: "Shape Type",
+        image: "../assets/games-img/ShapeType.png",
+        about: "A letter shaping game.",
+        alt: "Shape Type"
+    },
+  {
+        link: "https://color.method.ac/",
+        head: "Color",
+        image: "../assets/games-img/Color.png",
+        about: "A color matching game.",
+        alt: "Color"
+    },
+  {
+        link: "https://bezier.method.ac/",
+        head: "The Bezier Game",
+        image: "../assets/games-img/Bezier.png",
+        about: "A game to help you master the pen tool.",
+        alt: "The Bezier Game"
+    },
+  {
+        link: "https://www.supremo.co.uk/designers-eye/",
+        head: "It's Centred That",
+        image: "../assets/games-img/ItsCentredThat.png",
+        about: "Guess whether or not the dot is in the centre of each shape.",
+        alt: "It's Centred That"
+    },
+  {
+        link: "https://betterwebtype.com/triangle/",
+        head: "Better Web Type Triangle",
+        image: "../assets/games-img/Bezier.png",
+        about: "A game to learn typography.",
+        alt: "Better Web Type Triangle"
+    },
+  {
+        link: "http://www.flexboxdefense.com/",
+        head: "Flexbox Defense",
+        image: "../assets/games-img/Bezier.png",
+        about: "A game for learning CSS flexbox.",
+        alt: "Flexbox Defense"
+    },
+  {
+        link: "https://flexboxfroggy.com/",
+        head: "Flexbox Froggy",
+        image: "../assets/games-img/Bezier.png",
+        about: "A game for learning CSS flexbox.",
+        alt: "Flexbox Froggy"
+    },
+  {
+        link: "https://cssgridgarden.com/",
+        head: "Grid Garden",
+        image: "../assets/games-img/Bezier.png",
+        about: "A game for learning CSS grid.",
+        alt: "Grid Garden"
+    },
+  {
+        link: "https://flukeout.github.io/",
+        head: "CSS Diner",
+        image: "../assets/games-img/Bezier.png",
+        about: "Game about all kinds of CSS selectors.",
+        alt: "CSS Diner"
+    },
+  {
+        link: "https://vim-adventures.com/",
+        head: "VIM Adventures",
+        image: "../assets/games-img/Bezier.png",
+        about: "Learn VIM while playing a game.",
+        alt: "VIM Adventures"
+    },
+  {
+        link: "https://mystery.knightlab.com/",
+        head: "SQL Murder Mystery",
+        image: "../assets/games-img/Bezier.png",
+        about: "Fun way of learning SQL.",
+        alt: "SQL Murder Mystery"
+    },
+  {
+        link: "https://alexnisnevich.github.io/untrusted/",
+        head: "Untrusted",
+        image: "../assets/games-img/Bezier.png",
+        about: "A meta-JavaScript adventure game.",
+        alt: "Untrusted"
+    },
+  {
+        link: "https://warriorjs.com/",
+        head: "WarriorJS",
+        image: "../assets/games-img/Bezier.png",
+        about: "Code, prove your skills, and get hired.",
+        alt: "WarriorJS"
+    },
+  {
+        link: "https://lab.reaal.me/jsrobot/",
+        head: "JS Robot",
+        image: "../assets/games-img/Bezier.png",
+        about: "Learn JavaScript by playing a platform game.",
+        alt: "JS Robot"
+    },
+  {
+        link: "http://jsdares.com/",
+        head: "JS Dares",
+        image: "../assets/games-img/Bezier.png",
+        about: "Learn JavaScript by completing dares.",
+        alt: "JS Dares"
+    },
+  {
+        link: "https://www.crunchzilla.com/",
+        head: "Crunchzilla",
+        image: "../assets/games-img/Bezier.png",
+        about: "Play with code, experiment, build, and learn.",
+        alt: "Crunchzilla"
+    },
+  {
+        link: "https://www.crunchzilla.com/",
+        head: "Crunchzilla",
+        image: "../assets/games-img/Bezier.png",
+        about: "Play with code, experiment, build, and learn.",
+        alt: "Crunchzilla"
+    },
+];
+
+export default Games
---
+++ pages/games.js
@@ -0,0 +1,76 @@
+import Datas from "../components/games/Games-Data";
+import NoResults from "../components/NoResults";
+import { useState, useEffect, React } from "react";
+import { Link } from "react-scroll";
+import { useLocation } from "react-router-dom";
+import Card from "../components/Card/Card";
+
+const Content = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  // const { pathname } = useLocation();
+
+  // useEffect(() => {
+  //   window.scrollTo(0, 0);
+  // }, [pathname]);
+
+  const filterOptions =
+    searchTerm !== ""
+      ? Datas.filter((data) =>
+          data.head.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      : Datas;
+
+  return (
+    <>
+      <div className="container-landing">
+        <div className="landing-page-header">
+          <div className="header-details">
+            <h1>Games</h1>
+
+            <p>
+              
+            </p>
+
+            <Link to="container" smooth={true} duration={1000}>
+              <h4>Explore all</h4>
+            </Link>
+
+            <div className="search-container">
+              <i className="fa fa-search search-icon"></i>
+
+              <input
+                className="search"
+                text="type"
+                placeholder="Search"
+                onChange={(event) => {
+                  setSearchTerm(event.target.value);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container" id="container">
+        <div className="align-flex">
+          {filterOptions.length > 0 ? (
+            filterOptions.map((data, indx) => (
+              <Card
+                key={indx}
+                about={data.about}
+                alt={data.alt}
+                head={data.head}
+                image={data.image}
+                link={data.link}
+              />
+            ))
+          ) : (
+            <NoResults search={searchTerm} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Content;
