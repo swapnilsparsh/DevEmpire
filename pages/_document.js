16a17299f55971fa29d7f9188fffb4eec4d959ff
swapnilsparsh <arezona.lucky55@gmail.com>
2023-05-24 19:09:55 +0530

    GoogleAdSense added

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 //This file is created to assist dark mode implementation
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -21,7 +22,14 @@ class MyDocument extends Document {
     `;
     return (
       <Html>
-        <Head />
+        <Head>
+          <Script
+            async
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+            strategy="lazyOnload"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
           <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
           <Main />
