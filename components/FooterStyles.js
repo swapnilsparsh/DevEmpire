f5bd4eef244a50d9b677918868ec103eebdbaf98
Soumyajit Mondal <mondalsoumya02@gmail.com>
2023-05-27 21:02:26 +0000

    Added responsness

--- components/FooterStyles.js
+++ components/FooterStyles.js
@@ -1,23 +1,4 @@
 import styled from 'styled-components';
-
-// export const Box = styled.div`
-// padding: 80px 60px;
-// background: blue;
-// position: relative;
-// bottom: 0;
-// width: 100%;
-
-
-// @media (max-width: 1000px) {
-// 	padding: 70px 30px;
-// }
-// `;
-
-// export const Container = styled.div`
-// 	display:inline-block;
-// 	float:right;
-// `
-
 export const Column1 = styled.div`
 display: grid;
 width: 50%;
@@ -26,18 +7,6 @@ export const Column2 = styled.div`
 display: grid;
 width:50%;
 `;
-
-// export const Row = styled.div`
-// display: block;
-// grid-template-columns: repeat(auto-fill,
-// 						minmax(185px, 1fr));
-// grid-gap: 20px;
-
-// @media (max-width: 1000px) {
-// 	grid-template-columns: repeat(auto-fill,
-// 						minmax(200px, 1fr));
-// }
-// `;
 
 export const FooterLink = styled.a`
 color: #fff;
