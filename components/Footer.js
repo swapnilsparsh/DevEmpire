5d9029ed26912db646ec89c93bb0cb34ea0a7dd1
PARAS <mehtaparas1901@gmail.com>
2023-07-17 11:58:06 +0530

    added aria labels

--- components/Footer.js
+++ components/Footer.js
@@ -58,35 +58,34 @@ const Footer = () => {
           <div className="column1">
             <Column1>
               <Heading1>Support</Heading1>
-              <FooterLink
-                target="_blank"
-                href="https://github.com/swapnilsparsh/DevEmpire"
-              >
+              <FooterLink target="_blank" href="https://github.com/swapnilsparsh/DevEmpire" aria-label="Visit us on Github" rel="noopener noreferrer">
                 GitHub
               </FooterLink>
-              <FooterLink target="blank" href="https://www.producthunt.com/products/developer-empire?utm_source=badge-featured&utm_medium=badge#developer-empire">
+              <FooterLink target="blank" href="https://www.producthunt.com/products/developer-empire?utm_source=badge-featured&utm_medium=badge#developer-empire" aria-label="Product Hunt" rel="noopener noreferrer">
                 Product Hunt
               </FooterLink>
-              <FooterLink target="blank" href="https://twitter.com/intent/tweet?text=Check%20out%20this%20awesome%20website%20having%20a%20collection%20of%20all%20the%20campus%20ambassador%20and%20fellowship%20programs&url=https://devempire.netlify.app&via=swapnilsparsh">
+              <FooterLink target="blank" href="https://twitter.com/intent/tweet?text=Check%20out%20this%20awesome%20website%20having%20a%20collection%20of%20all%20the%20campus%20ambassador%20and%20fellowship%20programs&url=https://devempire.netlify.app&via=swapnilsparsh" aria-label="Visit us on Twitter" rel="noopener noreferrer">
                 Twitter
               </FooterLink>
-              <FooterLink href="#">Newsletter</FooterLink>
+              <FooterLink href="#" aria-label="Newsletter" rel="noopener noreferrer">
+                Newsletter
+              </FooterLink>
             </Column1>
           </div>
           <div className="column2">
             <Column2>
               <Heading2>Explore</Heading2>
               <Link href="/ambassador">
-                <FooterLink >Ambasaddor</FooterLink>
+                <FooterLink aria-label="Ambassador">Ambassador</FooterLink>
               </Link>
               <Link href="/programs">
-                <FooterLink>Programs</FooterLink>
+                <FooterLink aria-label="Programs">Programs</FooterLink>
               </Link>
               <Link href="/webdev">
-                <FooterLink >WebDev</FooterLink>
+                <FooterLink aria-label="WebDev">WebDev</FooterLink>
               </Link>
               <Link href="/games">
-                <FooterLink >Games</FooterLink>
+                <FooterLink aria-label="Games">Games</FooterLink>
               </Link>
             </Column2>
           </div>
