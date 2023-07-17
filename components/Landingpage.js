073318f42c67584e86a7cf36d137c22ff7bd8cb0
Deepanshu0703 <21bcs072@iiitdmj.ac.in>
2023-07-17 10:59:51 +0530

    Added hackathon on Landing page

--- components/Landingpage.js
+++ components/Landingpage.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import Home from "../public/assets/svg/Home.svg";
 import Ambassador from "../public/assets/svg/Ambassador.svg";
+import Hackathon from "../public/assets/svg/Hackathon.png";
 import WebDev from "../public/assets/svg/WebDev.svg";
 import Programs from "../public/assets/svg/Programs.svg";
 import Games from "../public/assets/svg/Gaming.svg";
@@ -73,64 +74,78 @@ const Landingpage = () => {
           </div>
           <div className="landing-page-details">
             <div className="heading-text second">
+              <Link href="/hackathons">
+                <h1>
+                  <u> Hackathons </u>
+                </h1>
+              </Link>
+              <p>
+              Participate in various hackathons to showcase your skills & get rewarded!              </p>
+            </div>
+            <div className="header-image">
+              <Image id="home" src={Hackathon} alt="Hackathon Illustration" />
+            </div>
+          </div>
+          <div className="landing-page-details">
+            <div className="header-image">
+              <Image id="home" src={Programs} alt="Programs Illustration" />
+              </div>
+            <div className="heading-text">
               <Link href="/programs">
                 <h1>
                   <u> Programs </u>
                 </h1>
               </Link>
               <p>
-                Looking for an open-source program to apply to? You're just a click away. Find all details right here!
+              Looking for an open-source program to apply to? You're just a click away. Find all details right here!
               </p>
-            </div>
-            <div className="header-image">
-              <Image id="home" src={Programs} alt="Programs Illustration" />
-            </div>
-          </div>
-          <div className="landing-page-details">
-            <div className="header-image">
-              <Image id="home" src={WebDev} alt="Web Dev Illustration" />
-            </div>
-            <div className="heading-text">
-              <Link href="/webdev">
-                <h1>
-                  <u> Web Dev Tools </u>
-                </h1>
-              </Link>
-              <p>
-                Power up your website with awesome tools from
-                CSS generators and stock photos, to icons, illustrations, vectors and so much more, all at one place
-              </p>
-            </div>
+              </div>
           </div>
           <div className="landing-page-details">
             <div className="heading-text second">
+              <Link href="/webdev">
+                <h1>
+                  <u> Web Dev </u>
+                </h1>
+              </Link>
+              <p>
+              Power up your website with awesome tools from
+                CSS generators and stock photos, to icons, illustrations, vectors and so much more, all at one place
+              </p>
+            </div>
+            <div className="header-image">
+              <Image id="home" src={WebDev} alt="Web Dev Illustration" />
+            </div>
+          </div>
+          <div className="landing-page-details">
+            <div className="header-image">
+              <Image id="home" src={Games} alt="Games Illustration" />
+            </div>
+            <div className="heading-text">
               <Link href="/games">
                 <h1>
                   <u> Games </u>
                 </h1>
               </Link>
               <p>
-                Ever heard of 'All work and no play makes the developer dull' ? Here's your chance to mix some play into your work !
+              Ever heard of 'All work and no play makes the developer dull' ? Here's your chance to mix some play into your work !
               </p>
-            </div>
-            <div className="header-image">
-              <Image id="home" src={Games} alt="Games Illustration" />
-            </div>
+              </div>
           </div>
           <div className="landing-page-details">
-            <div className="header-image">
-              <Image id="home" src={Backend} alt="Backend Illustration" />
-            </div>
-            <div className="heading-text">
+            <div className="heading-text second"> 
               <Link href="/backend">
                 <h1>
                   <u> Backend </u>
                 </h1>
               </Link>
               <p>
-                Looking for a backend service for your next project? We've got you covered with a list of the best ones out there!
+              Looking for a backend service for your project? We've got you covered with a list of the best ones out there!
               </p>
-            </div>
+              </div>
+            <div className="header-image">
+              <Image id="home" src={Backend} alt="Backend Illustration" />
+              </div>
           </div>
         </div>
       </div>
