7b0c16e240d2b7d5d8cbdc35f549cbc09c38982d
Nishitbaria <85815172+Nishitbaria@users.noreply.github.com>
2023-08-07 18:05:21 +0530

    Fixed Preloader

--- components/Preloader.js
+++ components/Preloader.js
@@ -3,7 +3,7 @@ import React from "react";
 const Preloader = ()  => {
     return(
     <div className="preload">
-        <img id="logo" src="../assets/icon/favicon.png" alt="" />
+        <img id="logo" src="../assets/icon/favicon.webp" alt="" />
     </div>
     )
 } 
