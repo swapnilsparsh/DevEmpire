d9f19afd59b42040e668f7afd1d74e8b398c6085
Nishitbaria <85815172+Nishitbaria@users.noreply.github.com>
2023-08-09 08:25:41 +0530

    fixed loader

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 3200);
+    }, 2200);
   }, []);
   return loading ? (
     <Preloader />
