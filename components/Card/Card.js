eda3437b5f2af1d1bd18d8c6b606ceb90d81f163
Jatin Yadav <jatin27yadav@gmail.com>
2024-01-06 23:07:51 +0530

    Link Key Added

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -10,29 +10,31 @@ const createMarkup = (html) => {
 
 function Card({ head, link, image, alt, about }) {
   return (
-    <div className="frame-border bg" key={`${head}_${link}`}>
-      <div className="pointer"></div>
-      <div className="card-js app">
-        <div className="content">
-          <img
-            src={image}
-            alt={alt}
-            loading="lazy"
-            decoding="async"
-            width="300" // Adjust the width according to your design
-          />
-          <h3 dangerouslySetInnerHTML={createMarkup(head)}></h3>
-          <p dangerouslySetInnerHTML={createMarkup(about)} style={{width: '75%', margin: '1rem auto'}}></p>
-          <Button variant="outlined">
-            <Link href={link} passHref>
-              <a className="glowing-button" target="_blank" rel="noreferrer">
-                View More
-              </a>
-            </Link></Button>
+    <>
+      <div className="frame-border bg" key={`${head}_${link}`}>
+        <div className="pointer"></div>
+        <div className="card-js app">
+          <div className="content">
+            <img
+              src={image}
+              alt={alt}
+              loading="lazy"
+              decoding="async"
+              width="300" // Adjust the width according to your design
+            />
+            <h3 dangerouslySetInnerHTML={createMarkup(head)}></h3>
+            <p dangerouslySetInnerHTML={createMarkup(about)} style={{ width: '75%', margin: '1rem auto' }}></p>
+            <Button variant="outlined">
+              <Link href={link} passHref key={`${head}_${link}`}>
+                <a className="glowing-button" target="_blank" rel="noreferrer">
+                  View More
+                </a>
+              </Link></Button>
+          </div>
         </div>
+        <div className="glow-border"></div>
       </div>
-      <div className="glow-border" ></div>
-    </div>
+    </>
   );
 }
 
