572fc95dcc96dbe5d9f797ca79903adc52ed8e5c
Jatin Yadav <jatin27yadav@gmail.com>
2024-01-23 12:20:48 +0530

    Fixed Console Errors

--- components/icons/navbarIcons.tsx
+++ components/icons/navbarIcons.tsx
@@ -28,11 +28,11 @@ export const AmbassadorIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <g fill="none" stroke="currentColor" stroke-width="2">
+    <g fill="none" stroke="currentColor" strokeWidth="2">
       <circle cx="12" cy="7" r="5" />
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M17 14h.352a3 3 0 0 1 2.976 2.628l.391 3.124A2 2 0 0 1 18.734 22H5.266a2 2 0 0 1-1.985-2.248l.39-3.124A3 3 0 0 1 6.649 14H7"
       />
     </g>
